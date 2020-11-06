c40623859e1a6e99809a327ac474e59d26d99899
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-06 11:17:32 -0800

    add html skeleton

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,15 @@
 
 // Document Ready:
 $(document).ready(function() {
-  $("form#FORMNAME").submit(function(e) {
+  $("form#build-a-pizza").submit(function(e) {
     e.preventDefault();
   })
 })
+
+// retrieve input from html:
+// `$("element#id-name")`
+// `$("input:TYPE[NAME OR ID=THE NAME OR THE ID]:checked")`
+// To get all elements from a multiple-choice question:
+  // $("input:[all that stuff above]:checked").each(function () {
+  //   defaultPizza.toppings = $(this).val();
+  // });
