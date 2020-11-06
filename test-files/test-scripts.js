7fb514a2bd21b1bb0a573dc16131deac7e2a4e0d
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-06 12:07:56 -0800

    add PizzaCart object and prototypes

--- test-files/test-scripts.js
+++ test-files/test-scripts.js
@@ -1,3 +1,21 @@
+function PizzaCart() {
+  this.pizzaList = [];
+  this.pizzaID = 0;
+}
+
+PizzaCart.prototype.addPizza = function(pizza) {
+  pizza.id = this.idThePizza();
+  this.pizzaList.push(pizza);
+}
+
+PizzaCart.prototype.idThePizza = function() {
+  this.pizzaID += 1;
+  return this.pizzaID;
+}
+
+
+
+
 function Pizza(size, crust, sauce, cheese, meat, veggie) {
   this.size = size;
   this.crust = crust;
@@ -6,3 +24,23 @@ function Pizza(size, crust, sauce, cheese, meat, veggie) {
   this.meat = meat;
   this.veggie = veggie;
 }
+
+Pizza.prototype.calculateBill = function() {
+  return (this.size * .90 ) + (this.meat.length -1) + ((this.veggie.length -1) * 0.75)
+}
+
+
+
+// retrieve input from html:
+`$("element#id-name")`
+`$("input:TYPE[NAME OR ID=THE NAME OR THE ID]:checked")`
+// To get all elements from a multiple-choice question:
+  $("input:[all that stuff above]:checked").each(function () {
+    questionArray = $(this).val();
+  });
+// to get it all from a single-choice:
+    questionArray = $("input#question-name").val();
+// to set these things
+let customerPizza = new Pizza(the, individually, named, arrays)
+
+  
