2ebaacbead98b97175e2d14d2dfd41e7dedc5aa3
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-06 12:35:14 -0800

    fill in names and values of pizza components

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,3 @@ $(document).ready(function() {
   })
 })
 
-// retrieve input from html:
-// `$("element#id-name")`
-// `$("input:TYPE[NAME OR ID=THE NAME OR THE ID]:checked")`
-// To get all elements from a multiple-choice question:
-  // $("input:[all that stuff above]:checked").each(function () {
-  //   defaultPizza.toppings = $(this).val();
-  // });
