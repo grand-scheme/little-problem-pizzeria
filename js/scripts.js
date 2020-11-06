41ef016ed936d0c565ee204048915f36c24eb2d9
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-06 13:24:51 -0800

    verify values are transfered between UI logic html to B logic jquery

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,30 @@ $(document).ready(function() {
   $("form#build-a-pizza").submit(function(e) {
     e.preventDefault();
   })
+
+  $("button#add-pizza-btn").click(function() {
+    let pSize = $("input:radio[name=pizza-size]:checked").val();
+    let pCrust = $("input:radio[name=pizza-crust]:checked").val();
+    let pSauce = $("input:radio[name=pizza-sauce]:checked").val();
+    let pCheese = $("input:radio[name=pizza-cheese]:checked").val();
+    let pProtein = [];
+    let pVeggie = [];
+
+    $("input:checkbox[name=pizza-protein]:checked").each(function () {
+      let proteinChoice = $(this).val();
+      pProtein.push(proteinChoice);
+    });
+    $("input:checkbox[name=pizza-veggie]:checked").each (function () {
+      let veggieChoice = $(this).val();
+      pVeggie.push(veggieChoice);
+    });
+
+    console.log(pSize);
+    console.log(pCrust);
+    console.log(pSauce);
+    console.log(pCheese);
+    console.log(pProtein);
+    console.log(pVeggie);
+  })
 })
 
