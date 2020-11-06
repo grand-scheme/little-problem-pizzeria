692038da6eacebcfc6a55609b27331abd0159280
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-06 15:16:23 -0800

    moved extant code to different line numbers

--- test-files/test-scripts.js
+++ test-files/test-scripts.js
@@ -50,20 +50,21 @@ let customerPizza = new Pizza(the, individually, named, arrays)
 $(document).ready(function() {
   $("form#build-a-pizza").submit(function(e) {
     e.preventDefault();
-  })
+  });
 
   $("button#add-pizza-btn").click(function() {
     let pSize = $("input:radio[name=pizza-size]:checked").val();
     let pCrust = $("input:radio[name=pizza-crust]:checked").val();
     let pSauce = $("input:radio[name=pizza-sauce]:checked").val();
     let pCheese = $("input:radio[name=pizza-cheese]:checked").val();
+    
     let pProtein = [];
-    let pVeggie = [];
-
     $("input:checkbox[name=pizza-protein]:checked").each(function () {
       let proteinChoice = $(this).val();
       pProtein.push(proteinChoice);
     });
+    
+    let pVeggie = [];
     $("input:checkbox[name=pizza-veggie]:checked").each (function () {
       let veggieChoice = $(this).val();
       pVeggie.push(veggieChoice);
@@ -75,4 +76,6 @@ $(document).ready(function() {
     console.log(pCheese);
     console.log(pProtein);
     console.log(pVeggie);
-  })
+  });
+});
+
