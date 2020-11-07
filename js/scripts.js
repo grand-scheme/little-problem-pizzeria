551f6155e079fdfacf7deaeabd63b120f9b52a5b
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-06 18:18:59 -0800

    pass tests for IDing pizza and passing to cart.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-// Build the Pizza
+// Build the PIZZACART ---
+function PizzaCart() {
+  this.pizzaList = [];
+  this.pizzaID = 0;
+}
+
+PizzaCart.prototype.addPizza = function(pizza) {
+  pizza.id = this.idThisPizza(); 
+  this.pizzaList.push(pizza);
+}
+
+PizzaCart.prototype.idThisPizza = function() {
+  this.pizzaID += 1;
+  return this.pizzaID;
+}
+
+// Build the PIZZA ---
 function Pizza(size, crust, sauce, cheese, meat, veggie) {
   this.size = size;
   this.crust = crust;
@@ -7,3 +23,13 @@ function Pizza(size, crust, sauce, cheese, meat, veggie) {
   this.meat = meat;
   this.veggie = veggie;
 }
+
+
+
+// this stuff would be user interface stuff when we get there
+let pizzaCart = new PizzaCart();
+let pizza = new Pizza("big", "crunch", "red", "w", "pep", "olive");
+let pizza2 = new Pizza(1,2,3,4,5,6);
+pizzaCart.addPizza(pizza);
+pizzaCart.addPizza(pizza2);
+pizzaCart
--- test-files/test-scripts.js
+++ test-files/test-scripts.js
@@ -20,30 +20,68 @@
 // }
 // so that one works.
 
-//  
+//  ------------- //
 
 // build the pizza cart
-function PizzaCart() {
-  this.pizzaList = [];
-}
+// function PizzaCart() {
+//   this.pizzaList = [];
+// }
 
-PizzaCart.prototype.addPizza = function(pizza) {
-  this.pizzaList.push(pizza);
-}
+// PizzaCart.prototype.addPizza = function(pizza) {
+//   this.pizzaList.push(pizza);
+// }
 
-function Pizza(size, crust, sauce, cheese, meat, veggie) {
-    this.size = size;
-    this.crust = crust;
-    this.sauce = sauce;
-    this.cheese = cheese;
-    this.meat = meat;
-    this.veggie = veggie;
-  }
-let pizzaCart = new PizzaCart();
-let pizza = new Pizza("big", "crunch", "red", "w", "pep", "olive");
-let pizza2 = new Pizza(1,2,3,4,5,6);
-pizzaCart.addPizza(pizza);
-pizzaCart.addPizza(pizza2);
-pizzaCart
+// function Pizza(size, crust, sauce, cheese, meat, veggie) {
+//   this.size = size;
+//   this.crust = crust;
+//   this.sauce = sauce;
+//   this.cheese = cheese;
+//   this.meat = meat;
+//   this.veggie = veggie;
+// }
+// let pizzaCart = new PizzaCart();
+// let pizza = new Pizza("big", "crunch", "red", "w", "pep", "olive");
+// let pizza2 = new Pizza(1,2,3,4,5,6);
+// pizzaCart.addPizza(pizza);
+// pizzaCart.addPizza(pizza2);
+// pizzaCart
 
-//okay and this set works....... still not sure what problem was with previous code??? will look.
+//okay and this set works....... still not sure what problem was with previous code??? 
+
+//  ------------- //
+
+// // Build the PIZZACART ---
+// function PizzaCart() {
+//   this.pizzaList = [];
+//   this.pizzaID = 0;
+// }
+
+// PizzaCart.prototype.addPizza = function(pizza) {
+//   pizza.id = this.idThisPizza(); 
+//   this.pizzaList.push(pizza);
+// }
+
+// PizzaCart.prototype.idThisPizza = function() {
+//   this.pizzaID += 1;
+//   return this.pizzaID;
+// }
+
+// // Build the PIZZA ---
+// function Pizza(size, crust, sauce, cheese, meat, veggie) {
+//   this.size = size;
+//   this.crust = crust;
+//   this.sauce = sauce;
+//   this.cheese = cheese;
+//   this.meat = meat;
+//   this.veggie = veggie;
+// }
+
+// // this stuff would be user interface stuff when we get there
+// let pizzaCart = new PizzaCart();
+// let pizza = new Pizza("big", "crunch", "red", "w", "pep", "olive");
+// let pizza2 = new Pizza(1,2,3,4,5,6);
+// pizzaCart.addPizza(pizza);
+// pizzaCart.addPizza(pizza2);
+// pizzaCart
+
+// this all works. cool.
