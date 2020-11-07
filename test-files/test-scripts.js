f2950d607a09fe07a8f55bb062447f5412f96de7
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-06 20:28:04 -0800

    add a couple more tests

--- test-files/test-scripts.js
+++ test-files/test-scripts.js
@@ -88,50 +88,64 @@
 
 //  ------------- //
 
-// Build the PIZZACART ---
-function PizzaCart() {
-  this.pizzaList = [];
-  this.pizzaID = 0;
-}
+// // Build the PIZZACART ---
+// function PizzaCart() {
+//   this.pizzaList = [];
+//   this.pizzaID = 0;
+// }
 
-PizzaCart.prototype.addPizza = function(pizza) {
-  pizza.pzID = this.idThisPizza(); 
-  this.pizzaList.push(pizza);
-}
-PizzaCart.prototype.idThisPizza = function() {
-  this.pizzaID += 1;
-  return this.pizzaID;
-}
+// PizzaCart.prototype.addPizza = function(pizza) {
+//   pizza.pzID = this.idThisPizza(); 
+//   this.pizzaList.push(pizza);
+// }
+// PizzaCart.prototype.idThisPizza = function() {
+//   this.pizzaID += 1;
+//   return this.pizzaID;
+// }
 
-PizzaCart.prototype.locatePizza = function(pzID) {
-  for (let i=0; i< this.pizzaList.length; i++) {
-    if (this.pizzaList[i].pzID == pzID) {
-      return this.pizzaList[i];
-    }
-  };
-  return false;
-}
+// PizzaCart.prototype.locatePizza = function(pzID) {
+//   for (let i=0; i< this.pizzaList.length; i++) {
+//     if (this.pizzaList[i]) {
+//       if (this.pizzaList[i].pzID == pzID) {
+//       return this.pizzaList[i];
+//       }
+//     }
+//   };
+//   return false;
+// }
 
-// Build the PIZZA ---
-function Pizza(size, crust, sauce, cheese, meat, veggie) {
-  this.size = size;
-  this.crust = crust;
-  this.sauce = sauce;
-  this.cheese = cheese;
-  this.meat = meat;
-  this.veggie = veggie;
-}
+// PizzaCart.prototype.destroyPizza = function(pzID) {
+//   for (let i=0; i< this.pizzaList.length; i++) {
+//     if (this.pizzaList[i]) {
+//         if (this.pizzaList[i].pzID == pzID) {
+//         delete this.pizzaList[i];
+//         return true;
+//       }
+//     }
+//   };
+//   return false;
+// }
+
+// // Build the PIZZA ---
+// function Pizza(size, crust, sauce, cheese, meat, veggie) {
+//   this.size = size;
+//   this.crust = crust;
+//   this.sauce = sauce;
+//   this.cheese = cheese;
+//   this.meat = meat;
+//   this.veggie = veggie;
+// }
 
 
 
-// this stuff would be user interface stuff when we get there
-let pizzaCart = new PizzaCart();
-let pizza = new Pizza("big", "crunch", "red", "w", "pep", "olive");
-let pizza2 = new Pizza(1,2,3,4,5,6);
-pizzaCart.addPizza(pizza);
-pizzaCart.addPizza(pizza2);
-pizza2;
-pizza;
+// // this stuff would be user interface stuff when we get there
+// let pizzaCart = new PizzaCart();
+// let pizza = new Pizza("big", "crunch", "red", "w", "pep", "olive");
+// let pizza2 = new Pizza(1,2,3,4,5,6);
+// pizzaCart.addPizza(pizza);
+// pizzaCart.addPizza(pizza2);
+// pizza2;
+// pizza;
   // pizzaCart.locatePizza(2) -->
   // {
   //   "size": 1,
@@ -143,4 +157,4 @@ pizza;
   //   "pzID": 2
   // }
 
-  // so this works too. sweet.
+  // so this works too. sweet!!! it goes into the main page.
